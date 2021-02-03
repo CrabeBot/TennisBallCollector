e77129db8860c18329c1a3a3da7beb0951bd17eb
Hamid HACENE <hamid.hacene@ensta-bretagne.org>
2021-02-03 08:48:01 +0100

    validation

--- image_saver/image_saver/save_image.py
+++ image_saver/image_saver/save_image.py
@@ -10,10 +10,10 @@ import rclpy.qos as qos
 class saver(Node):
     def __init__(self):
         super().__init__("saver")
-        self.profile1 = qos.QoSProfile(reliability=qos.QoSReliabilityPolicy.BEST_EFFORT)
+        self.profile1 = qos.QoSProfile(reliability=qos.QoSReliabilityPolicy.BEST_EFFORT, history=qos.QoSHistoryPolicy.KEEP_LAST, depth=5)
         self.im_subscriber = self.create_subscription(Image, "/zenith_camera/image_raw", self.im_callback, qos_profile=self.profile1)
         self.profile2 = qos.QoSProfile()
-        self.str_subscriber = self.create_subscription(Int32, "/cmd", self.str_callback, qos_profile=self.profile2)
+        self.str_subscriber = self.create_subscription(Int32, "/cmd", self.str_callback, 10)
         self.cmd = 0
         self.spin = True
 
