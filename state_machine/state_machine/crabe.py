d742889d6ccee4d5eeed239be871d18206d785bb
Corentin Lemoine <corentinlemoine22@gmail.com>
2021-02-24 09:39:40 +0100

    Crabe update

--- state_machine/state_machine/crabe.py
+++ state_machine/state_machine/crabe.py
@@ -1,101 +1,99 @@
 import tf2_ros
 import numpy as np
+from rclpy import Node
+from geometry_msgs.msg import Wrench, Point
+from std_msgs.msg import Float32, Float32MultiArray
+from rclpy.qos import qos_profile_services_default
+
 
 class Crabe():
 
-    def __init__(self, node) -> None:
+    def __init__(self, node: Node) -> None:
         self.node = node
+        
+        self.waypoints = []
+        self.balls = []
+        
         self.tfBuffer = tf2_ros.Buffer()
         self.listener = tf2_ros.TransformListener(self.tfBuffer, self)
+        self.pubBackDoor = node.create_publisher(Wrench, "/force_back_door", qos_profile_services_default)
+        self.pubFrontDoor = node.create_publisher(Wrench, "/force_front_door", qos_profile_services_default)
+        self.pubVel = node.create_publisher(Float32, "/vel", qos_profile_services_default)
+        self.pubPointA = node.create_publisher(Point, "/pointA", qos_profile_services_default)
+        self.pubPointB = node.create_publisher(Point, "/pointB", qos_profile_services_default)
+        self.pubTarget = node.create_publisher(Point, "/target", qos_profile_services_default)
 
+        node.create_subscription(Float32MultiArray, "/balls", self.__balls_callback)
+        node.create_subscription(Float32MultiArray, "/waypoints", self.__wp_callback)
+
+
+    def __balls_callback(self, msg):
+        self.balls = msg.data
+
+    def __wp_callback(self, msg):
+        wpData = msg.data
+        self.waypoints = wpData.reshape((-1,2))
+
+    def getPos(self):
+        transform  = self.tfBuffer.lookup_transform('odom', 'base_link', tf2_ros.Time())
+        return np.array([transform.translation.x, transform.translation.y])
+
+    def getTheta(self):
         transform  = self.tfBuffer.lookup_transform('odom', 'base_link', tf2_ros.Time())
         q = transform.transform.rotation
         siny_cosp = 2 * (q.w * q.z + q.x * q.y)
         cosy_cosp = 1 - 2 * (q.y * q.y + q.z * q.z)
         yaw = np.arctan2(siny_cosp, cosy_cosp)
-
-        u = self.control(transform.transform.translation.x, transform.transform.translation.y, yaw, self.a, self.b)
-
-    def getPos(self):
-        """
-        Lit la position du robot sur le topic correspondant et la renvoie au format np.array([[x, y]])
-        """
-        pass
-
-    def getTheta(self):
-        pass
-
-
-    def openFrontDoor(self):
-        """
-        Publie sur le topic correspondant le couple à appliquer pour ouvrir la porte avant
-        """
-        pass
+        return yaw
 
     def closeFrontDoor(self):
-        """
-        Publie sur le topic correspondant le couple à appliquer pour fermer la porte avant
-        """
-        pass
+        m = Wrench()
+        m.force.z = -50
+        self.pubFrontDoor.publish(m)
 
-
-    def openBackDoor(self):
-        """
-        Publie sur le topic correspondant le couple à appliquer pour ouvrir la porte arrière
-        """
-        pass
-
+    def openFrontDoor(self):
+        m = Wrench()
+        m.force.z = 50
+        self.pubFrontDoor.publish(m)
 
     def closeBackDoor(self):
-        """
-        Publie sur le topic correspondant le couple à appliquer pour fermer la porte arrière
-        """
-        pass
+        m = Wrench()
+        m.force.z = -50
+        self.pubBackDoor.publish(m)
+
+    def openBackDoor(self):
+        m = Wrench()
+        m.force.z = 50
+        self.pubBackDoor.publish(m)
 
     def getBalls(self):
-        """
-        Lit la liste des positions des balles détectées sur le topic correspondant 
-        et la renvoie au format np.array([[x1, y1], [x2, y2], ...])
-        """
-        pass
-
-
-    def setTarget(self, target):
-        """
-        Récupère la cible (au format np.array([[x, y]])) calculée par la fsm
-        et l'envoie au path planner via le topic correspondant
-        """
-        pass
-    
-
-    def getWaypoints(self) :
-        """
-        Lit la liste des waypoints calculés par le path_planner sur le topic correspondant 
-        et la renvoie au format np.array([[x1, y1], [x2, y2], ...])
-        """
-        pass
-
+        return self.balls
 
     def setLine(self, A, B):
-        """
-        Récupère les points A et B (au format np.array([[x, y]])) calculés par la fsm
-        et les envoie au contrôleur via le topic correspondant
-        """
-        pass
-
-
-    def isCatched():
-        """
-        Fonction retournant True si la balle target a été ramassée, False sinon.
-        On considère que la balle est ramassée si elle ne fait plus partie de la liste des balles détectées
-        """
+        m = Point()
+        m.x = A[0]
+        m.y = A[1]
+        self.pubPointA(m)
+        m.x = B[0]
+        m.y = B[1]
+        self.pubPointB(m)
+        
+    def setTarget(self, target):
+        m = Point()
+        x0, y0 = target
+        m.x = x0
+        m.y = y0
+        self.pubTarget.publish(m)
 
     def isIn(self):
-        """
-        Fonction retournant True si le robot se situe dans une des deux zones de décharge, False sinon.
-        """
-        pass
+        x, y = self.getPos()
 
+        return (x < -6.35 and y < -13) or (x > 6.35 and y > 13)
 
     def setSpeed(self, v):
-        pass
+        m = Float32()
+        m.data = v
+        self.pubVel.publish(m)
+
+    def getWaypoints(self):
+        return self.waypoints
